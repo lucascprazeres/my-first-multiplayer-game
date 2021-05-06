5432ac92dc132db0ede3e11ca0028c09d8e5a468
Lucas Prazeres <olucascostadp@gmail.com>
2021-05-06 18:42:02 -0300

    !ref: isolate keyboard listener factory on a esm

---
+++ src/keyboard-listener.js
@@ -0,0 +1,32 @@
+export default function createKeyboardListener() {
+  const state = {
+    observers: [],
+  }
+
+  function subscribe(observerFunction) {
+    state.observers.push(observerFunction)
+  }
+
+  function notifyAll(command) {
+    for (const observerFunction of state.observers) {
+      observerFunction(command)
+    }
+  }
+
+  addEventListener("keydown", handleKeydown)
+
+  function handleKeydown(event) {
+    const keyPressed = event.key
+
+    const command = {
+      playerId: 'player1',
+      keyPressed,
+    }
+
+    notifyAll(command)
+  }
+
+  return {
+    subscribe,
+  }
+}

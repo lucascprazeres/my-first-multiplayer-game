8ccbd5a2d2bd118b8aacf62fef53cdeaadff8493
Lucas Prazeres <olucascostadp@gmail.com>
2021-06-11 16:32:56 -0300

    add temporary method section

--- documentation/second-study/graphs/line.py
+++ documentation/second-study/graphs/line.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-measured_parameter = [12, 1, 7, 8, 11, 7, 7, 8] # requires 8 items
-number_of_players = [2,4,6,8,10,12,14,16]
+measured_parameter = [325.4999999999069,345.16000000014895,404.85000000018624,411.6699999999255,438.1] # requires 8 items
+number_of_players = [2,3,4,5,6]
 
-plt.plot(number_of_players, measured_parameter, marker="o")
-plt.title('parameter x Number of players')
+plt.plot(number_of_players, measured_parameter)
+plt.title('Delay (ms) x Number of players')
 plt.xlabel('Number of players')
-plt.ylabel('parameter')
+plt.ylabel('Delay (ms)')
 plt.show()
